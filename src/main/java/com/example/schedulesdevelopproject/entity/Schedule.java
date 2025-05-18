33372b5ac317827138bbd7e2468589c08c92e21d
Gi0000 <102645896+Gi0000@users.noreply.github.com>
2025-05-18 18:24:44 +0900

    Entity 설계

---
+++ src/main/java/com/example/schedulesdevelopproject/entity/Schedule.java
@@ -0,0 +1,41 @@
+package com.example.schedulesdevelopproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Schedule() {}
+
+    public Schedule(String username, String title, String contents) {
+        this.username = username;
+        this.title = title;
+        this.contents = contents;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/schedulesdevelopproject/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.schedulesdevelopproject.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+}
