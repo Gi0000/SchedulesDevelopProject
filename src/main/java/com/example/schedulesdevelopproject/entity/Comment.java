9cc03b6e3cf1cdad1f2c84c55898e61bd79483d3
Gi0000 <102645896+Gi0000@users.noreply.github.com>
2025-05-21 13:44:01 +0900

    lv7. 댓글 엔티티 생성

---
+++ src/main/java/com/example/schedulesdevelopproject/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.example.schedulesdevelopproject.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String commentContents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+
+}
